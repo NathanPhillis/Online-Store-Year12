57628a6da75179877265d5915228ec22af386fa1
NathanPhillis <nathan.phillis@yahoo.com.au>
2019-05-18 17:30:14 +0800

    Cleared out old junk

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
-
-if (workbox) {
-  console.log("mems");
-} else {
-  console.error(`Workbox has failed to load.`);
-}
